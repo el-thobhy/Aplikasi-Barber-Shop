9b57e1835405f0d53b976c84c39478097fa87054
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-06-23 06:50:17 +0700

    Memperbaharui relasi order service dan hapus unused import order team

--- barbershop/src/main/java/com/elthobhy/barbershop/datamodels/TransactionOrderServices.java
+++ barbershop/src/main/java/com/elthobhy/barbershop/datamodels/TransactionOrderServices.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,7 +26,7 @@ public class TransactionOrderServices {
     @JoinColumn(name = "orderId", nullable = false, insertable = false, updatable = false)
     private TransactionOrder transactionOrder;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "serviceId", nullable = false, insertable = false, updatable = false)
     private MasterServices masterServices;
 
